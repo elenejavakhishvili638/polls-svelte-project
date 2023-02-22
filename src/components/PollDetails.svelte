face802194a23d72b44e7548b5513d6d44a19fa8
elene638 <elenejavaxishvili@yahoo.com>
2023-02-22 10:49:49 -0800

    Handle voting bars style and vote percentages

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -6,6 +6,9 @@
 
   $: totalVotes = poll.votesA + poll.votesB;
 
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+
   const handleVote = (option, id) => {
     // console.log("sda");
     dispatch("handleVote", { option, id });
@@ -17,14 +20,12 @@
     <h3>{poll.question}</h3>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)} on:keypress>
-      <div class="percent percent-a">
-        <span>{poll.answerA} ({poll.votesA})</span>
-      </div>
+      <div class="percent percent-a" style="width: {percentA}%;" />
+      <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)} on:keypress>
-      <div class="percent percent-b">
-        <span>{poll.answerB} ({poll.votesB})</span>
-      </div>
+      <div class="percent percent-b" style="width: {percentB}%;" />
+      <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
 </Card>
@@ -52,5 +53,21 @@
   span {
     display: inline-block;
     padding: 10px 20px;
+  }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+
+  .percent-a {
+    border-left: 4px solid #d91b42;
+    background: rgba(217, 27, 66, 0.2);
+  }
+
+  .percent-b {
+    border-left: 4px solid #45c496;
+    background: rgba(69, 196, 150, 0.2);
   }
 </style>
